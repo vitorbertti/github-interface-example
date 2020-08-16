846a5654418766bb1059cf11fc4982903ea7f1ef
Vitor Bertti <berttivitor@gmail.com>
2020-08-16 14:12:43 -0300

    Created routes and global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <BrowserRouter>
+         <Header />
+         <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/:username" element={<Profile />} />
+            <Route path="/:username/:reponame" element={<Repo />} />
+         </Routes>
+
+         <GlobalStyles />
+      </BrowserRouter>
+   );
 }
 
 export default App;
---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Repo = () => {
+   return <h1>Profile</h1>;
+};
+
+export default Repo;
