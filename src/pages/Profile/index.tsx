dafa0e6ab8c8735922f96bc656eebe79aae71e0f
Vitor Bertti <berttivitor@gmail.com>
2020-08-18 16:34:28 -0300

    Created ProfileData component and added iconCSS

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
+
 const Profile = () => {
-   return <h1>Profile</h1>;
+   return (
+      <Container>
+         <Main>
+            <LeftSide>
+               <ProfileData
+                  username={'Username'}
+                  name={'Name'}
+                  avatarUrl={'url'}
+                  followers={11}
+                  following={3}
+                  company={'Company'}
+                  location={'Location'}
+                  email={'email'}
+                  blog={undefined}
+               />
+            </LeftSide>
+            <RightSide></RightSide>
+         </Main>
+      </Container>
+   );
 };
 
 export default Profile;
