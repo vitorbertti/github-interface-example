92327c0c9acea75f294c650420accd3bd35287e2
Vitor Bertti <berttivitor@gmail.com>
2020-08-26 11:51:47 -0300

    Added users API

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
@@ -7,24 +7,53 @@ import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading, RepoIcon,
 import ProfileData from '../../components/ProfileData';
 import RepoCard from '../../components/RepoCard';
 import RandomCalendar from '../../components/RandomCalendar';
+import { APIUser, APIRepo } from '../../@types';
+
+interface Data {
+   user?: APIUser;
+   repos?: APIRepo[];
+   error?: string;
+}
 
 const Profile = () => {
    const { username } = useParams();
+   const [data, setData] = useState<Data>();
 
    useEffect(() => {
       Promise.all([
          fetch(`https://api.github.com/users/${username}`),
          fetch(`https://api.github.com/users/${username}/repos`),
       ]).then(async (responses) => {
-         console.log(responses);
+         const [userResponse, reposResponse] = responses;
+
+         if (userResponse.status === 404) {
+            setData({ error: 'User not found' });
+            return;
+         }
+
+         const user = await userResponse.json();
+         const repos = await reposResponse.json();
+
+         const shuffledRepos = repos.sort(() => 0.5 - Math.random());
+         const slicedRepos = shuffledRepos.slice(0, 6);
+
+         setData({ user, repos: slicedRepos });
       });
    }, [username]);
+
+   if (data?.error) {
+      return <h1>{data.error}</h1>;
+   }
+
+   if (!data?.user || !data?.repos) {
+      return <h1>Loading...</h1>;
+   }
 
    const TabContent = () => (
       <div className="content">
          <RepoIcon />
          <span className="label">Repositories</span>
-         <span className="number">21</span>
+         <span className="number"> {data.user?.public_repos} </span>
       </div>
    );
 
@@ -41,15 +70,15 @@ const Profile = () => {
          <Main>
             <LeftSide>
                <ProfileData
-                  username={'Username'}
-                  name={'Name'}
-                  avatarUrl={'url'}
-                  followers={11}
-                  following={3}
-                  company={'Company'}
-                  location={'Location'}
-                  email={'email'}
-                  blog={undefined}
+                  username={data.user.login}
+                  name={data.user.name}
+                  avatarUrl={data.user.avatar_url}
+                  followers={data.user.followers}
+                  following={data.user.following}
+                  company={data.user.company}
+                  location={data.user.location}
+                  email={data.user.email}
+                  blog={data.user.blog}
                />
             </LeftSide>
             <RightSide>
@@ -61,15 +90,15 @@ const Profile = () => {
                   <h2>Random repos</h2>
 
                   <div>
-                     {[1, 2, 3, 4, 5, 6].map((n) => (
+                     {data.repos.map((item) => (
                         <RepoCard
-                           key={n}
-                           username={'username'}
-                           reponame={'reponame'}
-                           description={'description'}
-                           language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
-                           stars={5}
-                           forks={22}
+                           key={item.name}
+                           username={item.owner.login}
+                           reponame={item.name}
+                           description={item.description}
+                           language={item.language}
+                           stars={item.stargazers_count}
+                           forks={item.forks}
                         />
                      ))}
                   </div>
