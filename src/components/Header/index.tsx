10b61d2965c2059859d67f0a33bc226c5d76b09c
Vitor Bertti <berttivitor@gmail.com>
2020-08-27 09:32:42 -0300

    Added header search function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, GithubLogo, SearchForm } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+   const [search, setSearch] = useState('');
+   const navigate = useNavigate();
+
+   function handleSubmit(event: React.FormEvent) {
+      event.preventDefault();
+      navigate(`/${search.toLocaleLowerCase().trim()}`);
+   }
+
    return (
       <Container>
          <GithubLogo />
-         <SearchForm>
-            <input placeholder="Enter username or repo..." />
+         <SearchForm onSubmit={handleSubmit}>
+            <input
+               placeholder="Enter username or repo..."
+               value={search}
+               onChange={(e) => setSearch(e.currentTarget.value)}
+            />
          </SearchForm>
       </Container>
    );
